ee09fe892840d521160d09e1a0e86974f0fafc96
“danaremu” <dantaiyearemu@gmail.com>
2025-02-13 20:58:02 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='mobile-container'>
+        <div className='pix-card'>
+          <div className='img-card'></div>
+          <div className='img-text'>Watford Train: 21-02-2024</div>
+        </div>
+        <div className='pix-card'>
+          <div className='img-card'></div>
+          <div className='img-text'>Kings Cross: 17-10-2023</div>
+        </div>
+        <div className='pix-card'>
+          <div className='img-card'></div>
+          <div className='img-text'>Old Kent Road: 27-07-2024</div>
+        </div>
+        <div className='pix-card'>
+          <div className='img-card'></div>
+          <div className='img-text'>Enish Lewisham: 29-05-2024</div>
+        </div>
+        <div className='pix-card'>
+          <div className='img-card'></div>
+          <div className='img-text'>Enish Lewisham: 18-07-2023</div>
+        </div>
+        <div className='writeup'>
+          <span>
+            So glad I get to spend another valentine with the person I love. I
+            love you so much, my love. <br /> Happy Valentine's Day!
+          </span>
+          <br />
+          <span>Will you be my Valentine Omodolapo?</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
